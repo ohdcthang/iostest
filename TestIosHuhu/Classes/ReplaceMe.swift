82f72e3a334bd09770ad69afa465f106eb493845
thanghoduc <hoducthangtn2nhvt@gmail.com>
2024-05-03 12:13:29 +0700

    0.5.0

---
+++ TestIosHuhu/Classes/ReplaceMe.swift
@@ -0,0 +1,22 @@
+
+//
+//  Logger.swift
+//  TestIosHuhu
+//
+//  Created by Thang Ho Duc on 03/05/2024.
+//
+
+import Foundation
+
+
+public class Logger1{
+    public init(){}
+    
+    public func printLog (){
+        print("Hello victotia xao quyet")
+    }
+    
+    func privateMehthod(){
+        print("pricate")
+    }
+}
